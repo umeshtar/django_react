e57112e574a10f50b08092ab21c05b8fbd91b5d6
Umesh Tarsariya <117255351+umeshtar@users.noreply.github.com>
2024-10-10 19:58:46 +0530

    Update models.py
    Add Sample Models for Updated Schema

--- backend/app_system/models.py
+++ backend/app_system/models.py
@@ -88,7 +88,65 @@ class SystemConfiguration(RecurField):
     def __str__(self):
         return self.name
 
+""" For Sample """
+# class DjangoModelConfiguration(recur_field):
+#     name = models.CharField(max_length=100)
 
+#     def __str__(self):
+#         return self.name
+
+#     @staticmethod
+#     def allowed_models():
+#         return [
+#             f'{app}.{model}'
+#             for app, model_dict in apps.all_models.items()
+#             if app.startswith('App_')
+#             for model in model_dict
+#         ]
+
+#     def save(self, *args, **kwargs):
+#         if self.name not in self.allowed_models():
+#             raise ValidationError('Invalid Model Name, Must be App_Name.model_name')
+#         super().save(*args, **kwargs)
+
+
+# class MenuItem(recur_field):
+#     module_type_choices = [
+#         ('drop-down', 'drop-down'),
+#         ('navigation', 'navigation'),
+#         ('route', 'route'),
+#     ]
+#     name = models.CharField(max_length=100)
+#     module_type = models.CharField(max_length=50, choices=module_type_choices)
+
+#     # for module_type == 'navigation' or 'route'
+#     page_url = models.URLField(null=True, blank=True)
+#     django_models = models.ManyToManyField(DjangoModelConfiguration, blank=True, related_name='model_menu_items')
+#     routes = models.ManyToManyField('self', blank=True, related_name='route_menu_items', symmetrical=False)
+
+#     # for module_type == 'drop-down'
+#     children = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class CustomPermission(recur_field):
+#     name = models.CharField(max_length=200)
+#     codename = models.CharField(max_length=100)
+#     description = models.CharField(max_length=1000)
+
+#     element_type_choices = [
+#         ('Button', 'Button'),
+#     ]
+#     element_type = models.CharField(max_length=100, choices=element_type_choices, null=True, blank=True)
+
+#     users = models.ManyToManyField(CustomUser, blank=True, related_name='custom_permissions')
+#     groups = models.ManyToManyField(Group, blank=True, related_name='custom_permissions')
+#     django_models = models.ManyToManyField(DjangoModelConfiguration, blank=True, related_name='custom_permissions')
+
+#     def __str__(self):
+#         return self.name
 
 
 
