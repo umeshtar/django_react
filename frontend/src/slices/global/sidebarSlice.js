08e1f80566c5cf3c4adba95597c54ed56b16cdf9
umeshtar <umeshtarsariya36@gmail.com>
2025-03-08 00:01:41 +0530

    Permissions in progress

---
+++ frontend/src/slices/global/sidebarSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { baseAsyncThunk } from '../crud/crudSlice';
+import { authFetch } from '../../helpers/fetch';
+
+export function fetchSideBarData() {
+    return baseAsyncThunk({ name: 'sidebar', action: 'fetchSideBarData', func: (data) => authFetch.get('permission/sidebar') })
+}
+
+const sidebarSlice = createSlice({
+    name: 'sidebar',
+    initialState: {
+        data: [],
+        all_modules: [],
+        is_permission_manager: [],
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase('sidebar/fetchSideBarData/fulfilled', (state, action) => {
+                state.data = action.payload.data
+                state.all_modules = action.payload.all_modules
+                state.is_permission_manager = action.payload.is_permission_manager
+            })
+    }
+})
+
+export default sidebarSlice.reducer
+
+
